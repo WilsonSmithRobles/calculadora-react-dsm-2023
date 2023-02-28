320ced1b68f4a71784b34b903e5d6129273f1837
WilsonSmithRobles <robleschicawilsonsmith@gmail.com>
2023-02-28 08:28:23 +0100

    Calculadora terminada

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import NumField from './Components/NumField';
 function App() {
   return (
     <div id="main">
-      <p>Hello World!</p>
       <NumField></NumField>
     </div>
   );
--- src/Components/NumField.js
+++ src/Components/NumField.js
@@ -1,6 +1,5 @@
 import './NumField.css'
 import { useState } from 'react'
-//DUDAS: Si creo todos estos elementos con una id  establecida; al generarlos en el html del cliente no debería haber conflictos?
 
 const NumField = () => {
 
@@ -8,41 +7,51 @@ const NumField = () => {
     const [valorB, setValorB] = useState('');
     const [result, setResult] = useState('');
 
+    const button_classes = ["inactive_button", "active_button"];
+    const [button_types, setbutton_types] = useState([1, 0, 0, 0]);
+
     const valorA_handler = (event) => {
-        setValorA(event.target.value);
+        setValorA(parseInt(event.target.value));
     }
 
     const valorB_handler = (event) => {
-        setValorB(event.target.value);
+        setValorB(parseInt(event.target.value));
     }
 
-    const multiplyResult = (event, data) => {
+    const multiplyResult = (event) => {
         setResult(valorA * valorB);
+        setbutton_types([0, 0, 1, 0]);
     }
 
-    const addResult = (event, data) => {
+    const addResult = (event) => {
         setResult(parseInt(valorA) + parseInt(valorB));
+        setbutton_types([1, 0, 0, 0]);
     }
 
-    const subsResult = (event, data) => {
+    const subsResult = (event) => {
         setResult(valorA - valorB);
+        setbutton_types([0, 1, 0, 0]);
     }
     
-    const divResult = (event, data) => {
+    const divResult = (event) => {
         setResult(valorA / valorB);
+        setbutton_types([0, 0, 0, 1]);
     }
 
     return (
-        <div>
+        <div class="Calculadora">
+            <br></br>
+            <div>Calculadora React DSM-2023</div>
+            <br></br>
             <div>
                 <label>Número A:</label>
                 <input type='number' onChange={valorA_handler}></input>
             </div>
             <div >
-                <button onClick={addResult}>+</button>
-                <button onClick={subsResult}>-</button>
-                <button onClick={multiplyResult}>·</button>
-                <button onClick={divResult}>/</button>
+                <button onClick={addResult} class={button_classes[button_types[0]]}>+</button>
+                <button onClick={subsResult} class={button_classes[button_types[1]]}>-</button>
+                <button onClick={multiplyResult} class={button_classes[button_types[2]]}>*</button>
+                <button onClick={divResult} class={button_classes[button_types[3]]}>/</button>
             </div>
             <div>
                 <label>Número B:</label>
